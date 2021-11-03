e1a6c531cd3a4fca8d1097819f2eb0802bda2236
Jack Betts <42355170+JackBetts@users.noreply.github.com>
2021-11-03 21:52:15 +0000

    Adding tap and swipe to gesture controller

--- Scripts/Gestures/Scripts/GestureController.cs
+++ Scripts/Gestures/Scripts/GestureController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,10 @@ namespace VolcanicPig.Mobile.Gestures
 {
     public class GestureController : SingletonBehaviour<GestureController> 
     {
-        public delegate void TouchDown(Vector2 pos);
-        public static event TouchDown OnTouchDown;
-
-        public delegate void TouchUp(Vector2 pos);
-        public static event TouchUp OnTouchUp;
+        public static Action<Vector2> OnTouchDown;
+        public static Action<Vector2> OnTouchUp;
+        public static Action<Vector2> OnTap;
+        public static Action<Vector2> OnSwipe;
 
         private Vector2 _touchDelta;
         public Vector2 TouchDelta => _touchDelta;
@@ -19,10 +19,23 @@ namespace VolcanicPig.Mobile.Gestures
         public Vector2 ScaledTouchDelta => _scaledTouchDelta;
 
         private Vector2 _axis; 
-        public Vector2 Axis => _axis; 
+        public Vector2 Axis => _axis;
+
+        private bool _isTouching;
+        public bool Touching => _isTouching;
+
+        [Header("Swipe Settings")]
+        [SerializeField] private float swipeThreshold = 1f;
+
+        [Header("Tap Settings")]
+        [SerializeField] private float maxTapTime = 1f;
+        [SerializeField] private Vector2 minTapCancelDist = new Vector2(50, 50); 
 
         private Vector2 _lastMousePos;
-        private Vector2 _touchDownPos; 
+        private Vector2 _touchDownPos;
+        private Vector2 _touchUpPos;
+        private float _touchDownTime;
+        private bool _cancelTap; 
 
 
         private void Update()
@@ -43,11 +56,15 @@ namespace VolcanicPig.Mobile.Gestures
                 if (touch.phase == TouchPhase.Began)
                 {
                     _touchDownPos = touch.position; 
+                    _touchDownTime = Time.time;
+                    _isTouching = true;
+
                     OnTouchDown?.Invoke(touch.position);
                 }
 
                 if (touch.phase == TouchPhase.Moved)
                 {
+                    _touchUpPos = touch.position; 
                     Vector2 dragDistanceUnscaled = touch.deltaPosition;
 
                     _scaledTouchDelta = new Vector2(dragDistanceUnscaled.x / Screen.width,
@@ -55,11 +72,24 @@ namespace VolcanicPig.Mobile.Gestures
 
                     float _horizontalAxis = touch.position.x - _touchDownPos.x; 
                     float _verticalAxis = touch.position.y -_touchDownPos.y;
-                    _axis = new Vector2(_horizontalAxis, _verticalAxis).normalized; 
+                    _axis = new Vector2(_horizontalAxis, _verticalAxis).normalized;
+
+                    if (CheckSwipe())
+                    {
+                        _touchDownPos = touch.position; 
+                    }
                 }
 
                 if (touch.phase == TouchPhase.Ended)
                 {
+                    _touchUpPos = touch.position;
+                    _isTouching = false;
+
+                    if (DidTap(touch.position))
+                    {
+                        OnTap?.Invoke(touch.position); 
+                    }
+
                     OnTouchUp.Invoke(touch.position);
                 }
             }
@@ -77,14 +107,17 @@ namespace VolcanicPig.Mobile.Gestures
             {
                 Vector2 mousePos = Input.mousePosition;
                 _lastMousePos = mousePos;  
-                _touchDownPos = mousePos; 
+                _touchDownPos = mousePos;
+                _touchDownTime = Time.time;
+                _isTouching = true;
+
                 OnTouchDown?.Invoke(mousePos);
             }
 
             if (Input.GetMouseButton(0))
             {
                 Vector2 mousePos = Input.mousePosition;
-
+                _touchUpPos = mousePos; 
                 _touchDelta = mousePos - _lastMousePos;
                 _scaledTouchDelta = new Vector2(_touchDelta.x / Screen.width,
                     _touchDelta.y / Screen.height);
@@ -94,7 +127,12 @@ namespace VolcanicPig.Mobile.Gestures
                 float _horizontalAxis = mousePos.x - _touchDownPos.x; 
                 float _verticalAxis = mousePos.y -_touchDownPos.y;
 
-                _axis = new Vector2(_horizontalAxis, _verticalAxis).normalized;  
+                _axis = new Vector2(_horizontalAxis, _verticalAxis).normalized;
+
+                if (CheckSwipe())
+                {
+                    _touchDownPos = mousePos;
+                }
             }
             else
             {
@@ -105,8 +143,66 @@ namespace VolcanicPig.Mobile.Gestures
 
             if (Input.GetMouseButtonUp(0))
             {
+                Vector2 mousePos = Input.mousePosition; 
+                _touchUpPos = mousePos;
+                _isTouching = false;
+
+                if (DidTap(mousePos))
+                {
+                    OnTap?.Invoke(mousePos);
+                }
+
                 OnTouchUp?.Invoke(Input.mousePosition);
             }
+        }
+
+        private bool DidTap(Vector2 touchPos)
+        {
+            return Time.time < (_touchDownTime + maxTapTime) &&
+                (touchPos.x - _touchDownPos.x) < minTapCancelDist.x &&
+                (touchPos.y - _touchDownPos.y) < minTapCancelDist.y;
+        }
+
+        private bool CheckSwipe()
+        {
+            if (VerticalMove() > swipeThreshold && VerticalMove() > HorizontalMove())
+            {
+                if (_touchDownPos.y - _touchUpPos.y > 0)
+                {
+                    OnSwipe?.Invoke(new Vector2(0, -1));
+                    return true;
+                }
+                else
+                {
+                    OnSwipe?.Invoke(new Vector2(0, 1));
+                    return true;
+                }
+            }
+            else if (HorizontalMove() > swipeThreshold && HorizontalMove() > VerticalMove())
+            {
+                if (_touchDownPos.x - _touchUpPos.x > 0)
+                {
+                    OnSwipe?.Invoke(new Vector2(-1, 0));
+                    return true;
+                }
+                else
+                {
+                    OnSwipe?.Invoke(new Vector2(1, 0));
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private float VerticalMove()
+        {
+            return Mathf.Abs(_touchUpPos.y - _touchDownPos.y);
+        }
+
+        private float HorizontalMove()
+        {
+            return Mathf.Abs(_touchUpPos.x - _touchDownPos.x);
         }
     }
 }
